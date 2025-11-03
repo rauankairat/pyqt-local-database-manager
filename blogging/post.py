a78836a779be577eb36a31bb49c50067f9783442
Tugrul Basar Uysal <tuysal@uvic.ca>
2025-11-02 17:17:41 -0800

    fixed issue with post

--- blogging/post.py
+++ blogging/post.py
@@ -1,9 +1,9 @@
 import datetime
 
 class Post:
-    def __init__(self,code,title,text,creation,update):
+    def __init__(self,code,title,text):
         self.code = code
         self.title = title
         self.text = text
         self.creation = datetime.datetime.now()
-        self.updatee = datetime.datetime.now()
+        self.update = datetime.datetime.now()
