3cc7984e48e8c636c5aa099673f6a90a9bcf06b7
Tugrul Basar Uysal <tuysal@uvic.ca>
2025-11-20 14:02:22 -0800

    fixed more post errors

--- blogging/dao/post_dao_pickle.py
+++ blogging/dao/post_dao_pickle.py
@@ -11,50 +11,7 @@ class PostDAOPickle(PostDAO):
   
 
     def __init__(self):
-              
         self.posts = []      
-
-
-        if not os.path.exists(Configuration.records_path):
-            os.makedirs(Configuration.records_path)
-
-
-        self._load_from_file()
-
-
-
-    def _file_path(self) -> str:
-        return os.path.join(
-            Configuration.records_path,
-            f"{self.blog.id}{Configuration.records_extension}"
-        )
-
-    def _load_from_file(self) -> None:
-        path = self._file_path()
-        if not os.path.exists(path):
-            self.posts = []
-            if hasattr(self.blog, "post_count"):
-                self.blog.post_count = 0
-            return
-
-        with open(path, "rb") as f:
-            try:
-                self.posts = pickle.load(f)
-            except:
-                self.posts = []
-
-        # Sync blog.counter
-        if hasattr(self.blog, "post_count"):
-            if self.posts:
-                self.blog.post_count = max(p.code for p in self.posts)
-            else:
-                self.blog.post_count = 0
-
-    def _save_to_file(self) -> None:
-        path = self._file_path()
-        with open(path, "wb") as f:
-            pickle.dump(self.posts, f)
-
 
 
     def search_post(self, key: int):
